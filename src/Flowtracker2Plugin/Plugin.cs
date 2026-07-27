0b2d64c8e86be58a0f7f58e09cde3a59d71de2cf
TobyODonnell-AIAU <toby.odonnell@aquaticinformatics.com>
2026-07-28 09:39:25 +1000

    AT-2451 Fix net10 exceptions, update appveyor image to 2026

--- src/Flowtracker2Plugin/Plugin.cs
+++ src/Flowtracker2Plugin/Plugin.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+using System;
+using System.IO;
+using System.Text;
 using FieldDataPluginFramework;
 using FieldDataPluginFramework.Context;
 using FieldDataPluginFramework.Results;
@@ -7,6 +9,16 @@ namespace FlowTracker2Plugin
 {
     public class Plugin : IFieldDataPlugin
     {
+        static Plugin()
+        {
+            // Included to avoid "System.NotSupportedException: No data is available for encoding 437." error in net10
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            // SonTek's DataFileComplete method can cause an "AssemblyLoadContext is unloading or was already unloaded" error in net10
+            // Keeps the UnitConverter type alive to avoid this error
+            GC.KeepAlive(typeof(UnitConversion.UnitConverter));
+        }
+
         public ParseFileResult ParseFile(Stream fileStream, IFieldDataResultsAppender fieldDataResultsAppender, ILog logger)
         {
             var parser = new DataFileParser(logger, fieldDataResultsAppender);
